1cf693230ef6279d47c347af7ead3fe30f5582f1
Ifechimenim Ikwukala-Mbas <mbas750@gmail.com>
2025-09-29 09:24:29 +0100

    data types

--- src/test/java/LearnJava.java
+++ src/test/java/LearnJava.java
@@ -1,5 +1,28 @@
-public class LearnJava {
-    public static void main(String[] args) {
+class LearnJava {
+    static void main(String[] args) {
         System.out.println("Hello World");
+
+
+//        Data Types
+        // Primitive Data Types
+        byte age = 125; // max value is 128, min value is -128
+        short height = 30_000; // max value is 32,768
+        int someAge = 20; // max value 2.14M
+        long bigNumber = 234567876; // large numbers
+        float decimals = 3.14f; // Single-precision decimal.
+        double pi = 3.14159265359; // Default for decimals(Higher precision
+        boolean isValid = true; // logical values
+        char grade = 'A'; // stores a single character
+
+        // Non-Primitive Data Types
+        String name = "Mbas";
+        int[] numbers = {1,2,3,4,5,6,7,8,9,0};
+        char[] alphabets = {'A', 'B', 'C', 'D', 'E'};
+        String[] names = {"Mbas", "Ernest", "Ifechimenim", "Uche"};
+
+        final int START_YEAR = 2016; // constant value that can't be changed
+
+
+        System.out.println(START_YEAR);
     }
 }
